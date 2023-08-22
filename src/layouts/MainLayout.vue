bb2124293fad73066afdc05e0e9965e14d5239f6
Johan Alain Pulgarin Baeza <jpulgarin@ucol.mx>
2023-08-21 20:10:33 -0600

    Se agrega el proyecto

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,117 @@
+<template>
+  <q-layout view="hHh lpR fff">
+    <q-header reveal bordered class="bg-primary text-white q-pb-lg" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title align="left">
+          <q-btn flat label="Inicio" />
+          <q-btn flat label="Vista" />
+          <q-btn flat label="Ayuda" />
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-btn class="q-ml-lg" flat icon="menu" @click="toggleRightDrawer" />
+        <q-btn-dropdown split flat icon="ti-email" label="Correo nuevo" class="bg-secondary" />
+        <q-btn flat to="/page2" icon="ti-trash" label="Eliminar" />
+        <q-btn flat to="/page2" icon="ti-archive" label="Archivar" />
+        <q-btn flat to="/page2" icon="ti-info-alt" label="Informar" />
+        <q-btn flat to="/page2" icon="ti-paint-roller" label="Limpiar" />
+        <!-- Agrega más botones según tus necesidades -->
+      </q-tabs>
+    </q-header>
+
+<!--DRAWNER-->
+    <q-drawer show-if-above side="left" bordered>
+      <!--FAVORITOS-->
+      <div class="q-pa-md" align="center">
+        <q-btn-dropdown flat label="Favoritos" @show="onFavoritosDropdownShow" @hide="onFavoritosDropdownHide">
+          <q-list class="custom-list">
+            <q-item clickable @click="onItemClick" >
+              <q-item-section>
+                <q-item-label><q-icon name="ti-email" /> Bandeja de entrada</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable @click="onItemClick">
+              <q-item-section>
+                <q-item-label><q-icon name="ti-new-window" /> Elementos enviados</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable @click="onItemClick">
+              <q-item-section>
+                <q-item-label><q-icon name="ti-pencil" /> Borradores</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+      </div>
+      <!--CARPETAS-->
+      <div class="q-pa-md" align="center">
+        <q-btn-dropdown flat label="Carpetas" :style="{ marginTop: dropdownMargin }">
+          <q-list>
+            <q-item clickable @click="onItemClick">
+              <q-item-section>
+                <q-item-label><q-icon name="ti-email" /> Bandeja de entrada</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable @click="onItemClick">
+              <q-item-section>
+                <q-item-label><q-icon name="ti-pencil" /> Borradores</q-item-label>
+              </q-item-section>
+            </q-item>
+
+            <q-item clickable @click="onItemClick">
+              <q-item-section>
+                <q-item-label><q-icon name="ti-new-window" /> Elementos enviados</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+      </div>
+    </q-drawer>
+
+    <q-drawer show-if-above side="right" bordered width="1100">
+      <!-- drawer content -->
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      leftDrawerOpen: false,
+      favoritosDropdownOpen: false,
+      dropdownMargin: '0'
+    }
+  },
+  methods: {
+    onFavoritosDropdownShow () {
+      this.favoritosDropdownOpen = true
+      this.adjustCarpetasDropdownPosition()
+    },
+    onFavoritosDropdownHide () {
+      this.favoritosDropdownOpen = false
+      this.adjustCarpetasDropdownPosition()
+    },
+    adjustCarpetasDropdownPosition () {
+      if (this.favoritosDropdownOpen) {
+        this.dropdownMargin = '130px'
+      } else {
+        this.dropdownMargin = '0'
+      }
+    }
+  }
+}
+</script>
+
+<style >
+
+</style>
---
+++ src/pages/adminCorreos.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="q-pa-md">
+    <q-card class="my-card" flat bordered>
+      <q-tabs v-model="tab" class="text-teal">
+        <q-tab label="Prioritarios" name="one" />
+        <q-tab label="Todos" name="two" />
+      </q-tabs>
+
+      <q-separator />
+
+      <q-tab-panels v-model="tab" animated>
+        <q-tab-panel name="one">
+          <q-list bordered>
+            <q-item v-for="contact in contacts" :key="contact.id" class="q-my-sm" clickable v-ripple>
+              <q-item-section avatar>
+                <q-avatar color="primary" text-color="white">
+                  {{ contact.letter }}
+                </q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label>{{ contact.name }}</q-item-label>
+                <q-item-label caption lines="1">{{ contact.email }}</q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-tab-panel>
+
+        <q-tab-panel name="two">
+          With so much content to display at once, and often so little screen real-estate,
+          Cards have fast become the design pattern of choice for many companies, including
+          the likes of Google and Twitter.
+        </q-tab-panel>
+      </q-tab-panels>
+    </q-card>
+  </div>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      tab: ref('one'),
+      contacts
+    }
+  }
+}
+
+const contacts = [{
+  id: 1,
+  name: 'Ruddy Jedrzej',
+  email: 'rjedrzej0@discuz.net',
+  letter: 'R'
+}]
+
+</script>
+
+<style lang="scss" scoped>
+.my-card{
+  display: flex;
+  flex-direction: column;
+}
+</style>
